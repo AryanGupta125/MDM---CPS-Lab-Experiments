9e0b408eed6888b566963a206c5f98fededbc396
Aryan Gupta <aryanayush082005@gmail.com>
2026-07-20 17:16:00 +0530

    Add files via upload

---
+++ Expt. 3/Serial_Console/Serial_Console.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // Open the serial communication link at 9600 bits per second (baud)
+  Serial.begin(9600);  
+}
+
+void loop() {
+  Serial.println("Hello World / Aryan Gupta");
+  delay(1000);
+}
