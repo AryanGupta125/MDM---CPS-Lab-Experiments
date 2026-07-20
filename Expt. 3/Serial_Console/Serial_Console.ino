66f0d136c58d23d15fc353bd4d470d178b4268ac
Aryan Gupta <aryanayush082005@gmail.com>
2026-07-20 17:15:12 +0530

    Delete Expt. 3/Serial_Console directory

--- Expt. 3/Serial_Console/Serial_Console.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // Open the serial communication link at 9600 bits per second (baud)
-  Serial.begin(9600);  
-}
-
-void loop() {
-  Serial.println("Hello World / Aryan Gupta");
-  delay(1000);
-}
